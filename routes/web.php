2ef025b2a9be3cd26f55786fe2ceff536d749d3c
Shahed Amer <chanyeol6521354@gmail.com>
2024-06-25 15:14:12 +0200

    the routes for the edit and delete added

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,12 @@ Route::get('/distributors', [DistributorController::class, 'index'])->name('dist
 // link naar de show methode van de DistributorController en daar laat die een view zien met de data van de distributor met het id dat je meegeeft
 Route::get('/distributors/{id}', [DistributorController::class, 'show'])->name('distributors.show');
 Route::get('/distributor/{id}', 'DistributorController@show')->name('distributor.show');
+// link naar de create methode van de DistributorController en daar laat die een view zien met een formulier om een nieuwe distributor toe te voegen
+Route::delete('/distributors/{distributor}', [DistributorController::class, 'destroy'])->name('distributors.destroy');
+// Voor het ophalen van het bewerkingsformulier
+Route::get('/distributors/{distributor}/edit', [DistributorController::class, 'edit'])->name('distributors.edit');
+// Voor het verwerken van de wijzigingen
+Route::put('/distributors/{distributor}', [DistributorController::class, 'update'])->name('distributors.update');
+// Voor het opslaan van de nieuwe leverancier
+Route::get('/distributors/create',  [DistributorController::class, 'create'])->name('distributors.create');
 require __DIR__ . '/auth.php';

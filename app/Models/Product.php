5990998cffeb9cfee4633b495b6131a76a7fa28d
Igone van_der_Kley <igone@vdkley.nl>
2024-06-25 15:02:18 +0200

    Query added to model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -28,4 +28,11 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class, 'category_id');
     }
+
+    public static function getProductsWithCategory()
+    {
+        return self::join('categories', 'categories.id', '=', 'products.category_id')
+            ->select('products.name', 'products.id', 'products.category_id', 'products.amount', 'products.barcode', 'products.description', 'products.distributors_id', 'products.created_at', 'products.updated_at', 'categories.name as category_name')
+            ->get();
+    }
 }

9751cc872e05973bf88ea3ed5ac919cc4f71be93
Quinty <quintydewinter1@gmail.com>
2024-06-25 15:11:17 +0200

    model added for packages

---
+++ app/Models/Packages.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Packages extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'user_id', 'date_created', 'date_picked_up'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'products_per_package');
+    }
+}

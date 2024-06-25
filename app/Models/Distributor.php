9250a8284bce0b84e02e3575105f9d8c1e65f92a
Shahed Amer <chanyeol6521354@gmail.com>
2024-06-25 13:04:50 +0200

    a realations add between products an distributors

--- app/Models/Distributor.php
+++ app/Models/Distributor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Distributor extends Model
@@ -18,4 +19,17 @@ class Distributor extends Model
         'country',
         'next_delivery',
     ];
+    public static function distributorWithProducts($id)
+    {
+        return DB::table('distributors')
+            ->join('products', 'distributors.id', '=', 'products.distributors_id')
+            ->where('distributors.id', $id)
+            ->select('distributors.*', 'products.name as productName', 'products.description as productDescription')
+            ->get();
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'distributors_id');
+    }
 }
